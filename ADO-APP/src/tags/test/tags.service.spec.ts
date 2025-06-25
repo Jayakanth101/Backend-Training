d792bb57a105cf756f55aac5011b9748c9aa0bfd
User <user@Root-admins-MacBook-Air.local>
2025-06-25 18:32:16 +0530

    unit-test/tag module

---
+++ ADO-APP/src/tags/test/tags.service.spec.ts
@@ -0,0 +1,69 @@
+import { Test } from "@nestjs/testing"
+import { CreateTagDto } from "../dto/create-tag-dto"
+import { Tags } from "../tag.entity"
+import { TagService } from "../tag.service"
+import { getRepositoryToken } from "@nestjs/typeorm"
+import { create } from "domain"
+import { Repository } from "typeorm"
+
+describe('TagService', () => {
+    let service: TagService;
+    let repo: Repository<Tags>;
+    const mockTag: Tags = {
+        id: 1,
+        tagname: "sample",
+        workitems: []
+    }
+
+    const mockTagDto: CreateTagDto = {
+        tagname: "sample"
+    }
+
+    const mockTagRepo = {
+        create: jest.fn(),
+        save: jest.fn(),
+        findOneBy: jest.fn(),
+        delete: jest.fn(),
+        findBy: jest.fn()
+    }
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                TagService,
+                {
+                    provide: getRepositoryToken(Tags),
+                    useValue: mockTagRepo,
+                }
+            ]
+        }).compile();
+
+        service = module.get<TagService>(TagService);
+        repo = module.get<Repository<Tags>>(getRepositoryToken(Tags));
+
+    });
+
+    it('it creates tag', async () => {
+        mockTagRepo.create.mockReturnValue(mockTag);
+        mockTagRepo.save.mockResolvedValue(mockTag);
+
+        const result = await service.createTag(mockTagDto);
+
+        expect(mockTagRepo.create).toHaveBeenCalledWith(mockTagDto);
+        expect(mockTagRepo.save).toHaveBeenCalledWith(mockTag);
+
+        expect(result).toEqual(mockTag);
+    });
+
+    it('it delete a tag', async () => {
+        mockTagRepo.findOneBy.mockResolvedValue({ id: mockTag.id });
+        mockTagRepo.delete.mockResolvedValue(mockTag.id);
+
+        const result = await service.deleteTag(mockTag.id);
+
+        expect(mockTagRepo.findOneBy).toHaveBeenCalledWith({ id: mockTag.id });
+        expect(mockTagRepo.delete).toHaveBeenCalledWith(mockTag.id);
+
+        expect(result).toEqual(`Comment ${mockTag.id} is deleted`);
+    });
+})
